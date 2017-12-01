0e184553cf1d203627916d6515f9905f17e4b9aa
xuxufeng <1936131489@qq.com>
2017-12-01 11:03:56 +0800

    解决viewpager图片放大和缩小时出现的 java.lang.IllegalArgumentException: pointerIndex out of range问题的解决方法。

---
+++ viewpager_bug/ViewPagerXu.java
@@ -0,0 +1,39 @@
+package com.zhengb.phonelive.widget;
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+
+/**
+ *  解决图片放大缩小出错的bug
+ */
+public class ViewPagerXu extends ViewPager {
+    public ViewPagerXu(Context context) {
+        super(context);
+    }
+
+    public ViewPagerXu(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        try {
+            return super.onTouchEvent(ev);
+        } catch (IllegalArgumentException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        try {
+            return super.onInterceptTouchEvent(ev);
+        } catch (IllegalArgumentException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+}
